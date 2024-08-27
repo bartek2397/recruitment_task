5e3c1c34ac3cb039e444c5a4ee5bd9abd0bc74d2
Bartek <bartosz.wszolek97@gmail.com>
2024-08-27 18:28:37 +0200

    final fixes and optimizing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-// import App from './App.tsx'
 import './index.css'
 import './App.css'
 import Products from './routes/products.tsx'
